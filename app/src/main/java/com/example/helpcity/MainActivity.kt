43b6b718d9319a9c92456c1310c05f4abb32c431
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-02-27 18:13:18 +0000

    Adding Landscape, Testing strings.xml and adding project logos

--- app/src/main/java/com/example/helpcity/MainActivity.kt
+++ app/src/main/java/com/example/helpcity/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.helpcity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+     //   Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
     }
 }
