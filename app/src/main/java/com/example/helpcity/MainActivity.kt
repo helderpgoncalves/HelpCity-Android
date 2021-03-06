cc5ffb01c0bc215d1d31b4e774175326b59d5494
Hélder Gonçalves <helderpimentaxc@gmail.com>
2021-03-06 12:56:27 +0000

    Bottom Nav Bar now working and changing Fragment to Fragment

--- app/src/main/java/com/example/helpcity/MainActivity.kt
+++ app/src/main/java/com/example/helpcity/MainActivity.kt
@@ -1,13 +1,39 @@
 package com.example.helpcity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.helpcity.fragments.MapFragment
+import com.example.helpcity.fragments.NotesFragment
+import com.example.helpcity.fragments.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mapFragment = MapFragment()
+    private val notesFragment = NotesFragment()
+    private val profileFragment = ProfileFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-     //   Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
+        replaceFragment(mapFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.map -> replaceFragment(mapFragment)
+                R.id.notes -> replaceFragment(notesFragment)
+                R.id.profile -> replaceFragment(profileFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
